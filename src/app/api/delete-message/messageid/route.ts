6fc516d87f26dd1bd834339dd571f9f85dc1d892
Garvita Asthana <garvitasthana11@gmail.com>
2025-11-01 10:05:54 +0530

    add delete-message endpoint

---
+++ src/app/api/delete-message/messageid/route.ts
@@ -0,0 +1,60 @@
+import dbConnect from "@/lib/dbConnect";
+import { getServerSession, User } from "next-auth";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import UserModel from "@/models/user.model";
+import { AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function DELETE(
+  request: Request,
+  {
+    params,
+  }: {
+    params: { messageid: string };
+  }
+) {
+  await dbConnect();
+
+  const { messageid } = params;
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+
+  if (!session || !user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  try {
+    const updatedResult = await UserModel.updateOne(
+      { _id: user._id },
+      {
+        $pull: { messages: { _id: messageid } },
+      }
+    );
+
+    if (updatedResult.modifiedCount === 0) {
+      return Response.json(
+        {
+          success: false,
+          message: "Message not found",
+        },
+        { status: 404 }
+      );
+    }
+  } catch (error) {
+    const axiosError = error as AxiosError<ApiResponse>;
+
+    return Response.json(
+      {
+        success: false,
+        message: axiosError.response?.data.message,
+      },
+      { status: 500 }
+    );
+  }
+}
